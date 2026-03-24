777f0c6bc2a27fe36d0ca6ed7a4a8c4c5dfeb75e
chanikan <chanikan.n91@gmail.com>
2026-03-24 23:46:51 +0800

    api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
-})
-
+  baseURL: 'https://chinesedictionary.onrender.com/api', 
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
     if(token) {
