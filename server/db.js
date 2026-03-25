31e2f4db5f730a80dc8063c76afe691f37956111
chanikan <chanikan.n91@gmail.com>
2026-03-25 21:55:39 +0800

    db.js

--- server/db.js
+++ server/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
+    ssl: { rejectUnauthorized: false },
     connectionLimit: 10
 })
 
